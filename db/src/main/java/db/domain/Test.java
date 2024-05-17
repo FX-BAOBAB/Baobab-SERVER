41cd07663faf9b2b88d10ae72fe6079527064a86
JIUNG YANG <yangjiung286@gmail.com>
2024-05-17 20:18:24 +0900

    initial commit

---
+++ db/src/main/java/db/domain/Test.java
@@ -0,0 +1,5 @@
+package db.domain;
+// TODO 삭제 예정 클래스입니다.
+public class Test {
+
+}
---
+++ receiving/src/main/java/receiving/ReceivingApplication.java
@@ -0,0 +1,11 @@
+package receiving;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ReceivingApplication {
+    public static void main(String[] args) {
+        SpringApplication.run(ReceivingApplication.class, args);
+    }
+}
