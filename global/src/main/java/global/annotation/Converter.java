dd031e6214139a8cfb3b5329801b0f5e5633da23
shinyeongwoon <duddns119@naver.com>
2024-05-21 14:34:54 +0900

    SB-32 (feat) : @Converter Annotation 구현
    SB-32 (feat) : @Converter Annotation 구현
    
    Request DTO → Entity 로 변환,
    Entity → Response DTO 로 변환
    등, 특정 Class 변환, 특정 Type 변환 의 책임을 가지는 Class 명시를 위한 Annotation

---
+++ global/src/main/java/global/annotation/Converter.java
@@ -0,0 +1,14 @@
+package global.annotation;
+
+import java.lang.annotation.ElementType;
+import java.lang.annotation.Retention;
+import java.lang.annotation.RetentionPolicy;
+import java.lang.annotation.Target;
+import org.springframework.stereotype.Component;
+
+@Target(ElementType.ANNOTATION_TYPE)
+@Retention(RetentionPolicy.RUNTIME)
+@Component
+public @interface Converter {
+
+}
