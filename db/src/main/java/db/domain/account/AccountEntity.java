2871f57b31859e517ad462238b124bebe0bff45b
SEOB <svj7531@naver.com>
2024-05-23 17:01:47 +0900

    SB-50 (feat) : Account Repository 개발
    SB-50 (feat) : Account Repository 개발
    
    1. AccountEntity 개발
    2. AccountRepository 개발

---
+++ db/src/main/java/db/domain/account/AccountEntity.java
@@ -0,0 +1,47 @@
+package db.domain.account;
+
+import db.common.BaseEntity;
+import db.domain.address.AddressEntity;
+import db.domain.users.UserEntity;
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.FetchType;
+import jakarta.persistence.JoinColumn;
+import jakarta.persistence.OneToMany;
+import jakarta.persistence.OneToOne;
+import jakarta.persistence.Table;
+import java.util.List;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.EqualsAndHashCode;
+import lombok.NoArgsConstructor;
+import lombok.experimental.SuperBuilder;
+
+@Entity
+@Table(name = "account")
+@SuperBuilder
+@Data
+@EqualsAndHashCode(callSuper = true)
+@NoArgsConstructor
+@AllArgsConstructor
+public class AccountEntity extends BaseEntity {
+
+    @Column(nullable = false, length = 100)
+    private String email;
+
+    @Column(nullable = false, length = 100)
+    private String password;
+
+    @Column(nullable = false, length = 50, columnDefinition = "VARCHAR(50)")
+    private String name;
+
+    @OneToOne(fetch = FetchType.LAZY)
+    @JoinColumn(name = "user_id")
+    private UserEntity user;
+
+    @OneToMany(fetch = FetchType.LAZY)
+    @JoinColumn(name="address_id")
+    private List<AddressEntity> address;
+
+
+}
