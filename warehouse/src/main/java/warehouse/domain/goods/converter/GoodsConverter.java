1126a40bd38ce362fcbd7f7691cd5e1a59985373
shinyeongwoon <duddns119@naver.com>
2024-05-26 10:17:25 +0900

    SB-64 (feat) : goods 등록 개발
    SB-64 (feat) : goods 등록 개발
    
    1. GoodsConverter 구현
    2. GoodsService 구현

--- warehouse/src/main/java/warehouse/domain/goods/converter/GoodsConverter.java
+++ warehouse/src/main/java/warehouse/domain/goods/converter/GoodsConverter.java
@@ -1,10 +1,70 @@
 package warehouse.domain.goods.converter;
 
+import db.domain.goods.GoodsEntity;
+import db.domain.image.ImageEntity;
 import global.annotation.Converter;
+import java.util.ArrayList;
+import java.util.List;
+import java.util.stream.Collectors;
 import lombok.RequiredArgsConstructor;
+import warehouse.domain.goods.controller.model.GoodsRequest;
+import warehouse.domain.goods.controller.model.GoodsResponse;
+import warehouse.domain.image.controller.model.ImageListRequest;
+import warehouse.domain.image.controller.model.ImageListResponse;
+import warehouse.domain.image.controller.model.ImageResponse;
 
 @Converter
 @RequiredArgsConstructor
 public class GoodsConverter {
 
+    public GoodsEntity toEntity(GoodsRequest request) {
+        return GoodsEntity.builder()
+            .name(request.getName())
+            .modelName(request.getModelName())
+            .category(request.getCategory())
+            .quantity(request.getQuantity())
+            .build();
+    }
+
+    public GoodsResponse toResponse(GoodsEntity goodsEntity, ImageListResponse basic, ImageListResponse fault) {
+        return GoodsResponse.builder()
+            .id(goodsEntity.getId())
+            .name(goodsEntity.getName())
+            .basicImages(basic.getImageResponseList())
+            .faultImages(fault.getImageResponseList())
+            .category(goodsEntity.getCategory())
+            .quantity(goodsEntity.getQuantity())
+            .build();
+    }
+
+    public List<GoodsResponse> toResponseList(List<GoodsEntity> goodsEntityList, List<ImageListResponse> basicImageListReponseList, List<ImageListResponse> faultImageListReponseList) {
+
+        List<GoodsResponse> responses = new ArrayList<>();
+        for (int i = 0 ; i < goodsEntityList.size() ; i++){
+            GoodsResponse response = toResponse(goodsEntityList.get(i),
+                basicImageListReponseList.get(i), faultImageListReponseList.get(i));
+            responses.add(response);
+        }
+        return responses;
+    }
+
+    public List<GoodsResponse> toResponseList(List<GoodsEntity> goodsEntityList, List<ImageListResponse> basicImageListReponseList) {
+        List<GoodsResponse> responses = new ArrayList<>();
+        for (int i = 0 ; i < goodsEntityList.size() ; i++){
+            GoodsResponse response = toResponse(goodsEntityList.get(i),basicImageListReponseList.get(i));
+            responses.add(response);
+        }
+        return responses;
+    }
+
+    private GoodsResponse toResponse(GoodsEntity goodsEntity, ImageListResponse basic) {
+        return GoodsResponse.builder()
+            .id(goodsEntity.getId())
+            .name(goodsEntity.getName())
+            .basicImages(basic.getImageResponseList())
+            .faultImages(null)
+            .category(goodsEntity.getCategory())
+            .quantity(goodsEntity.getQuantity())
+            .build();
+    }
 }
--- warehouse/src/main/java/warehouse/domain/goods/service/GoodsService.java
+++ warehouse/src/main/java/warehouse/domain/goods/service/GoodsService.java
@@ -1,5 +1,6 @@
 package warehouse.domain.goods.service;
 
+import db.domain.goods.GoodsEntity;
 import db.domain.goods.GoodsRepository;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
@@ -10,4 +11,14 @@ public class GoodsService {
 
     private final GoodsRepository goodsRepository;
 
+    public GoodsEntity save(GoodsEntity goodsEntity,Long receivingId) {
+        // TODO user system 구현 완료 시 사용자 아이디 입력 필요
+        goodsEntity.setReceivingId(receivingId);
+        goodsEntity.setUserId(1L);
+        return goodsRepository.save(goodsEntity);
+    }
+
+    public GoodsEntity findByReceivingIdGoodsWithThrow(Long receivingId) {
+        return goodsRepository.findAllByReceivingIdOrderByIdDesc(receivingId);
+    }
 }
