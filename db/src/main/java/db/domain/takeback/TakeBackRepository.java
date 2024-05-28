82349b2b42d61561ce2aa109d59081d5104e5ffa
shinyeongwoon <duddns119@naver.com>
2024-05-28 18:07:20 +0900

    SB-82 (feat) : 반품 DB 구축
    SB-82 (feat) : 반품 DB 구축
    
    TakeRepository 구현

---
+++ db/src/main/java/db/domain/takeback/TakeBackRepository.java
@@ -0,0 +1,8 @@
+package db.domain.takeback;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface TakeBackRepository extends JpaRepository<TakeBackEntity,Long> {
+    
+}
+
