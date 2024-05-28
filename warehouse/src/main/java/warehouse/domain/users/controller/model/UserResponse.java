91010fcc00a2429c3a789e6e8f7dcdbdba64e532
Jhynguu <jhyngu0531@gmail.com>
2024-05-28 23:45:00 +0900

    SB-80 (feat) : UserResponse 개발
    응답 기능 개발

---
+++ warehouse/src/main/java/warehouse/domain/users/controller/model/UserResponse.java
@@ -0,0 +1,20 @@
+package warehouse.domain.users.controller.model;
+
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+@Builder
+public class UserResponse {
+
+    private String id;
+
+    private String email;
+
+    private String name;
+
+}
