f0e2e9614277a612f2f0e47732487670ec74750c
Jhynguu <jhyngu0531@gmail.com>
2024-05-29 17:38:59 +0900

    SB-73(Refactor) : UserErrorCode 수정
    에러 코드 수정

--- warehouse/src/main/java/warehouse/domain/users/errorcode/UserErrorCode.java
+++ warehouse/src/main/java/warehouse/domain/users/errorcode/UserErrorCode.java
@@ -9,7 +9,7 @@ import org.springframework.http.HttpStatus;
 @Getter
 public enum UserErrorCode implements ErrorCodeIfs {
 
-    NOT_FOUND_USER(404, 1103, "존재하지 않는 사용자입니다."),
+    NOT_FOUND_USER(404, 1102, "존재하지 않는 사용자입니다."),
     USER_ALREADY_EXISTS(HttpStatus.CONFLICT.value(), 1104, "이미 존재하는 사용자입니다."),
     INVALID_USER_DATA(HttpStatus.BAD_REQUEST.value(), 1105, "잘못된 사용자 데이터입니다.");
 
