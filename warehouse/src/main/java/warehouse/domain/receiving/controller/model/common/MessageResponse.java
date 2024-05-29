cf300f30b60bd69180ab58e6dfe1467915e16589
shinyeongwoon <duddns119@naver.com>
2024-05-29 11:06:24 +0900

    SB-84 (feat) : Massage Response 구현
    SB-84 (feat) : Massage Response 구현
    
    공통 massage Response 구현

---
+++ warehouse/src/main/java/warehouse/domain/receiving/controller/model/common/MessageResponse.java
@@ -0,0 +1,16 @@
+package warehouse.domain.receiving.controller.model.common;
+
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+@Builder
+public class MessageResponse {
+
+    private String Message;
+
+}
