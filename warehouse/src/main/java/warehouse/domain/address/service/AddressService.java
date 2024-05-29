7b98c1241ced082ffb51fd2e0832f99b1c4adda8
SEOB <svj7531@naver.com>
2024-05-29 16:36:37 +0900

    SB-75 (Refactor) 주소 조회 예외 처리 구현
    SB-75 (Refactor) 주소 조회 예외 처리 구현
    
    데이터가 없을 때 "주소 정보를 찾을 수 없습니다." 응답

--- warehouse/src/main/java/warehouse/domain/address/service/AddressService.java
+++ warehouse/src/main/java/warehouse/domain/address/service/AddressService.java
@@ -2,13 +2,13 @@ package warehouse.domain.address.service;
 
 import db.domain.address.AddressEntity;
 import db.domain.address.AddressRepository;
-import db.domain.users.UserEntity;
 import db.domain.users.UserRepository;
 import db.domain.users.enums.UserStatus;
 import java.util.List;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.stereotype.Service;
+import warehouse.domain.address.exception.addressexception.NotFoundAddressException;
 
 @Service
 @RequiredArgsConstructor
@@ -24,7 +24,11 @@ public class AddressService {
             //TODO 유저 상태에 따른 로직 처리
         }
 
-        return addressRepository.findByUserId(userId);
+        List<AddressEntity> addressEntityList = addressRepository.findByUserId(userId);
+        if (addressEntityList.isEmpty()) {
+            throw new NotFoundAddressException();
+        }
+        return addressEntityList;
     }
 
     public AddressEntity findBasicAddress(Long userId) {
@@ -35,6 +39,10 @@ public class AddressService {
 
         AddressEntity basicAddress = addressRepository.findByUserIdAndBasicAddress(
             userId, true);
+
+        if (basicAddress == null) {
+            throw new NotFoundAddressException();
+        }
 
         return basicAddress;
     }
