2ed1999d1a242f815201246d3aea05c4271f56ae
shinyeongwoon <duddns119@naver.com>
2024-05-31 13:28:34 +0900

    SB-90 (feat) : TakeBackResponse 구현
    SB-90 (feat) : TakeBackResponse 구현
    
    TakeBackResponse goods field 추가

--- warehouse/src/main/java/warehouse/domain/takeback/controller/model/TakeBackResponse.java
+++ warehouse/src/main/java/warehouse/domain/takeback/controller/model/TakeBackResponse.java
@@ -2,10 +2,13 @@ package warehouse.domain.takeback.controller.model;
 
 import db.domain.takeback.enums.TakeBackStatus;
 import java.time.LocalDateTime;
+import java.util.List;
 import lombok.AllArgsConstructor;
 import lombok.Builder;
 import lombok.Data;
 import lombok.NoArgsConstructor;
+import lombok.experimental.SuperBuilder;
+import warehouse.domain.goods.controller.model.GoodsResponse;
 
 @Data
 @NoArgsConstructor
@@ -16,5 +19,6 @@ public class TakeBackResponse {
     private Long id;
     private TakeBackStatus status;
     private LocalDateTime takeBackRequestAt;
+    private List<GoodsResponse> goods;
 
 }
