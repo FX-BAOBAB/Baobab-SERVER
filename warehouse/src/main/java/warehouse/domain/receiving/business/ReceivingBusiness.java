0e028a1a62256826fec61baec8179d035f6efe18
shinyeongwoon <duddns119@naver.com>
2024-06-02 09:29:05 +0900

    SB-92 (Refactor) : Receiving Business Refactoring
    SB-92 (Refactor) : Receiving Business Refactoring
    
    1. imageBusiness 책임 누수 Refactoring
     - imageBusiness -> imageService 변경
    2. Converter Package Refactoring
     - Guarantee , Message Package 분리

--- warehouse/src/main/java/warehouse/domain/receiving/business/ReceivingBusiness.java
+++ warehouse/src/main/java/warehouse/domain/receiving/business/ReceivingBusiness.java
@@ -1,9 +1,12 @@
 package warehouse.domain.receiving.business;
 
 import db.domain.goods.GoodsEntity;
+import db.domain.image.ImageEntity;
+import db.domain.image.enums.ImageKind;
 import db.domain.receiving.ReceivingEntity;
 import global.annotation.Business;
 import java.util.List;
+import java.util.Map;
 import java.util.stream.Collectors;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
@@ -11,16 +14,18 @@ import warehouse.domain.goods.controller.model.GoodsRequest;
 import warehouse.domain.goods.controller.model.GoodsResponse;
 import warehouse.domain.goods.converter.GoodsConverter;
 import warehouse.domain.goods.service.GoodsService;
-import warehouse.domain.image.business.ImageBusiness;
 import warehouse.domain.image.common.ImageUtils;
 import warehouse.domain.image.controller.model.ImageListResponse;
 import warehouse.domain.image.converter.ImageConverter;
+import warehouse.domain.image.service.ImageService;
 import warehouse.domain.receiving.controller.model.common.MessageResponse;
 import warehouse.domain.receiving.controller.model.guarantee.GuaranteeResponse;
 import warehouse.domain.receiving.controller.model.receiving.ReceivingRequest;
 import warehouse.domain.receiving.controller.model.receiving.ReceivingResponse;
 import warehouse.domain.receiving.controller.model.receiving.ReceivingStatusResponse;
-import warehouse.domain.receiving.converter.ReceivingConverter;
+import warehouse.domain.receiving.converter.guarantee.GuaranteeConverter;
+import warehouse.domain.receiving.converter.message.MessageConverter;
+import warehouse.domain.receiving.converter.receiving.ReceivingConverter;
 import warehouse.domain.receiving.service.ReceivingService;
 
 @Slf4j
@@ -32,8 +37,10 @@ public class ReceivingBusiness {
     private final ReceivingConverter receivingConverter;
     private final GoodsService goodsService;
     private final GoodsConverter goodsConverter;
-    private final ImageBusiness imageBusiness;
+    private final ImageService imageService;
     private final ImageConverter imageConverter;
+    private final GuaranteeConverter guaranteeConverter;
+    private final MessageConverter messageConverter;
 
 
     public ReceivingResponse receivingRequest(ReceivingRequest request) {
@@ -51,11 +58,14 @@ public class ReceivingBusiness {
 
         Long goodsId = ImageUtils.getFirstGoodsId(newGoodsEntityList);
 
-        ImageListResponse imageListResponse = imageBusiness.receivingRequest(goodsRequests,
+        Map<ImageKind, List<ImageEntity>> imageMap = imageService.receivingRequest(goodsRequests,
             goodsId);
 
-        List<GoodsResponse> goodsResponseList = goodsConverter.toResponseListBy(
-            goodsEntityList, imageListResponse);
+        ImageListResponse imageListResponse = imageConverter.toImageListResponse(
+            imageMap.get(ImageKind.BASIC), imageMap.get(ImageKind.FAULT));
+
+        List<GoodsResponse> goodsResponseList = goodsConverter.toResponseListBy(goodsEntityList,
+            imageListResponse);
 
         return receivingConverter.toResponse(registeredReceivingEntity, goodsResponseList);
     }
@@ -74,17 +84,18 @@ public class ReceivingBusiness {
 
     public GuaranteeResponse setGuarantee(Long receivingId) {
         ReceivingEntity receivingEntity = receivingService.setGuarantee(receivingId);
-        return receivingConverter.toGuaranteeResponse(receivingEntity);
+        return guaranteeConverter.toGuaranteeResponse(receivingEntity);
     }
 
     public MessageResponse abandonment(Long receivingId) {
         // TODO Login User 의 소유가 아닌 물품인 경우 Exception 처리 필요
         goodsService.abandonment(receivingId);
-        return receivingConverter.toMassageResponse("소유권 전환이 완료되었습니다.");
+        return messageConverter.toMassageResponse("소유권 전환이 완료되었습니다.");
     }
 
     public MessageResponse abandonment(List<Long> goodsIdList) {
+        // TODO Login User 의 소유가 아닌 물품인 경우 Exception 처리 필요
         goodsService.abandonment(goodsIdList);
-        return receivingConverter.toMassageResponse("소유권 전환이 완료되었습니다.");
+        return messageConverter.toMassageResponse("소유권 전환이 완료되었습니다.");
     }
 }
