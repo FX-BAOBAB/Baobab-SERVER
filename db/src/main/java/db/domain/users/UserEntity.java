36d7fe7ac9265588580d040b645a55e1d711f770
SEOB <svj7531@naver.com>
2024-06-03 22:06:06 +0900

    SB-41 (Refactor) : UserEntity 수정
    SB-41 (Refactor) : UserEntity 수정
    
    1. @Column(nullable) 추가
    2. 객체 참조 제거

--- db/src/main/java/db/domain/users/UserEntity.java
+++ db/src/main/java/db/domain/users/UserEntity.java
@@ -1,20 +1,14 @@
 package db.domain.users;
 
 import db.common.BaseEntity;
-import db.domain.account.AccountEntity;
-import db.domain.address.AddressEntity;
 import db.domain.users.enums.UserRole;
 import db.domain.users.enums.UserStatus;
 import jakarta.persistence.Column;
 import jakarta.persistence.Entity;
 import jakarta.persistence.EnumType;
 import jakarta.persistence.Enumerated;
-import jakarta.persistence.FetchType;
-import jakarta.persistence.OneToMany;
-import jakarta.persistence.OneToOne;
 import jakarta.persistence.Table;
 import java.time.LocalDateTime;
-import java.util.List;
 import lombok.AllArgsConstructor;
 import lombok.Data;
 import lombok.EqualsAndHashCode;
@@ -38,16 +32,11 @@ public class UserEntity extends BaseEntity {
     @Enumerated(EnumType.STRING)
     private UserStatus status;
 
-
+    @Column(nullable = false)
     private LocalDateTime registeredAt;
 
     private LocalDateTime unregisteredAt;
 
     private LocalDateTime lastLoginAt;
 
-    @OneToOne(mappedBy = "user", fetch = FetchType.LAZY)
-    private AccountEntity account;
-
-//    @OneToMany(mappedBy = "user", fetch = FetchType.LAZY)
-//    private List<AddressEntity> address;
 }
