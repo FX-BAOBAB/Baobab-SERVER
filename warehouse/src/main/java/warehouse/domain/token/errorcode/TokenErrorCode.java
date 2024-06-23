6896733a747963fe9844a7a4392fb4bb694c0319
SEOB <svj7531@naver.com>
2024-06-24 01:41:10 +0900

    SB-72 (Refactor) : TokenErrorCode 수정
    SB-72 (Refactor) : TokenErrorCode 수정
    
    1. httpCode, errorCode 변경

--- warehouse/src/main/java/warehouse/domain/token/errorcode/TokenErrorCode.java
+++ warehouse/src/main/java/warehouse/domain/token/errorcode/TokenErrorCode.java
@@ -8,11 +8,11 @@ import lombok.Getter;
 @Getter
 public enum TokenErrorCode implements ErrorCodeIfs {
 
-    INVALID_TOKEN(400, 2000, "유효하지 않은 토큰입니다."),
-    EXPIRE_TOKEN(401, 1103, "Token 이 만료되었습니다."),
-    UNKNOWN_TOKEN_EXCEPTION(400, 2002, "알 수 없는 토큰입니다."),
-    AUTHORIZATION_TOKEN_NOT_FOUND(400, 2003, "인증 헤더 토큰이 없습니다."),
-    VALID_TOKEN(400, 2004, "만료되지 않은 토큰입니다.")
+    INVALID_TOKEN(401, 1105, "유효하지 않은 토큰입니다."),
+    EXPIRE_TOKEN(401, 1106, "Token 이 만료되었습니다."),
+    UNKNOWN_TOKEN_EXCEPTION(401, 1107, "알 수 없는 토큰입니다."),
+    AUTHORIZATION_TOKEN_NOT_FOUND(401, 1108, "인증 헤더 토큰이 없습니다."),
+    VALID_TOKEN(400, 1109, "만료되지 않은 토큰입니다.")
     ;
 
 
