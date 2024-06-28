1b4f074824d8b80d711e9dd8f53bcf35202d5506
shinyeongwoon <duddns119@naver.com>
2024-06-28 11:22:45 +0900

    SB-99 (feat) : Spring Security Config
    SB-99 (feat) : Spring Security Config
    
    1. 인증 PASS URL 설정
     - Resource, SWAGGER, open-api permitAll
    2. 그외 URL 인증 필요

---
+++ warehouse/src/main/java/warehouse/common/config/security/SecurityConfig.java
@@ -0,0 +1,38 @@
+package warehouse.common.config.security;
+
+import java.util.List;
+import org.springframework.boot.autoconfigure.security.servlet.PathRequest;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.Customizer;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.web.SecurityFilterChain;
+
+@Configuration
+@EnableWebSecurity // security 활성화
+public class SecurityConfig{
+
+    private final List<String> SWAGGER = List.of(
+        "/swagger-ui.html",
+        "/swagger-ui/**",
+        "/v3/api-docs/**"
+    );
+
+    @Bean
+    public SecurityFilterChain filterChain(HttpSecurity httpSecurity) throws Exception {
+
+        httpSecurity
+            .authorizeHttpRequests(it -> {
+                it
+                    .requestMatchers(PathRequest.toStaticResources().atCommonLocations()).permitAll()
+                    .requestMatchers(SWAGGER.toArray(new String[0])).permitAll()
+                    .requestMatchers("/open-api/**").permitAll()
+                    .anyRequest().authenticated();
+            })
+            .formLogin(Customizer.withDefaults());
+
+        return httpSecurity.build();
+    }
+
+}
