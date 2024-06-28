0eb13f27f9ecc4a470308c9bd94a7d251cc37c1e
shinyeongwoon <duddns119@naver.com>
2024-06-28 14:01:15 +0900

    SB-97 (fix) : 관리 entity 오류 수정
    SB-97 (fix) : 관리 entity 오류 수정
    
    AddressRepository -> AddressEntity 변경

--- db/src/main/java/db/domain/users/address/AddressRepository.java
+++ db/src/main/java/db/domain/users/address/AddressRepository.java
@@ -1,8 +1,7 @@
 package db.domain.users.address;
 
 import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.stereotype.Repository;
 
-public interface AddressRepository extends JpaRepository<AddressRepository,Long> {
+public interface AddressRepository extends JpaRepository<AddressEntity,Long> {
 
 }
