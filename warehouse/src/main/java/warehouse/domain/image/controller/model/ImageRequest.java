46e0eeaa53ae0f68bcac432c4cda032a1f0a3573
SEOB <svj7531@naver.com>
2024-07-02 20:14:58 +0900

    SB-93 (feat) : ImageRequest 유효성 검사 추가
    SB-93 (feat) : ImageRequest 유효성 검사 추가
    
    1. `private MultipartFile file;`
        - file 은 null, “” 허용하지 않음
    2. `private ImageKind kind;`
        - @NotNull : 해당 필드가 null을 가지지 못하도록 함 → custom validation 고려
    3. `private String caption;`
        - caption 은 null, “”, “ “ 허용하지 않음

--- warehouse/src/main/java/warehouse/domain/image/controller/model/ImageRequest.java
+++ warehouse/src/main/java/warehouse/domain/image/controller/model/ImageRequest.java
@@ -1,6 +1,9 @@
 package warehouse.domain.image.controller.model;
 
 import db.domain.image.enums.ImageKind;
+import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.NotEmpty;
+import jakarta.validation.constraints.NotNull;
 import lombok.AllArgsConstructor;
 import lombok.Builder;
 import lombok.Data;
@@ -13,12 +16,13 @@ import org.springframework.web.multipart.MultipartFile;
 @Builder
 public class ImageRequest {
 
-    //TODO Valid 필요
-
+    @NotEmpty
     private MultipartFile file;
 
+    @NotNull
     private ImageKind kind;
 
+    @NotBlank
     private String caption;
 
 }
