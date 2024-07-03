fc4faed0b120047a31d609ba15f02361d201b02e
SEOB <svj7531@naver.com>
2024-07-04 00:19:51 +0900

    SB-93 (Refactor) : GoodsRequest @Size 수정
    SB-93 (Refactor) : GoodsRequest @Size 수정
    
    1. @Size(min = 1, max = 20) -> @Size(max = 20) 으로 변경

--- warehouse/src/main/java/warehouse/domain/goods/controller/model/GoodsRequest.java
+++ warehouse/src/main/java/warehouse/domain/goods/controller/model/GoodsRequest.java
@@ -21,11 +21,11 @@ import org.springframework.web.multipart.MultipartFile;
 public class GoodsRequest {
 
     @NotBlank
-    @Size(min = 1, max = 20) //이름 길이
+    @Size(max = 20) //이름 길이
     private String name;
 
     @NotBlank
-    @Size(min = 1, max = 20) //이름 길이
+    @Size(max = 20) //이름 길이
     private String modelName;
 
     @NotNull
