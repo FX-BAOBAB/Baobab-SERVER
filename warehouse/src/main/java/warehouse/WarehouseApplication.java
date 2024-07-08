d03eaa4e63b0a5511dcd3e0fbbc1720356df87ce
shinyeongwoon <duddns119@naver.com>
2024-07-08 17:08:40 +0900

    SB-132 (!HOTFIX) : Spring Application Https 설정
    SB-132 (!HOTFIX) : Spring Application Https 설정
    
    1. SpringApplication `@OpenAPIDefinition` Annotation 추가

--- warehouse/src/main/java/warehouse/WarehouseApplication.java
+++ warehouse/src/main/java/warehouse/WarehouseApplication.java
@@ -1,10 +1,13 @@
 package warehouse;
 
+import io.swagger.v3.oas.annotations.OpenAPIDefinition;
+import io.swagger.v3.oas.annotations.servers.Server;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.context.properties.EnableConfigurationProperties;
 import warehouse.domain.image.properties.FileStorageProperties;
 
+@OpenAPIDefinition(servers = {@Server(url = "https://baobab.run", description = "fx server 연습용 도메인")})
 @SpringBootApplication
 @EnableConfigurationProperties({
     FileStorageProperties.class
