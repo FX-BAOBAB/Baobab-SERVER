1af04299808d2e8a3caf7fe36e7c281b901f15f8
shinyeongwoon <duddns119@naver.com>
2024-07-11 10:52:41 +0900

    SB-164 (!HOTFIX) : 필수 Header ( TOKEN ) 값이 전달되지 않은 경우 메시지 응답 없음
    SB-164 (!HOTFIX) : 필수 Header ( TOKEN ) 값이 전달되지 않은 경우 메시지 응답 없음
    
    1. ErrorMessage 정보 누락으로 수정

--- global/src/main/java/global/errorcode/ErrorCode.java
+++ global/src/main/java/global/errorcode/ErrorCode.java
@@ -13,8 +13,9 @@ public enum ErrorCode implements ErrorCodeIfs{
     INTERNAL_SERVER_ERROR(HttpStatus.INTERNAL_SERVER_ERROR.value(),500,"서버 내부에 에러가 발생했습니다."),
     NULL_POINT(HttpStatus.INTERNAL_SERVER_ERROR.value(), 512,"NULL POINT 입니다."),
     INVALID_INPUT_DATA(HttpStatus.BAD_REQUEST.value(), 400,"유효하지 않은 입력값입니다."),
-    MISSING_REQUIRED_HEADER(HttpStatus.BAD_REQUEST.value(), 431,"필수 헤더값이 누락되었습니다."),
+    MISSING_REQUIRED_HEADER(HttpStatus.BAD_REQUEST.value(), 431,"필수 헤더값이 누락 또는 만료되었습니다."),
     MISSING_REQUIRED_PARAM(HttpStatus.BAD_REQUEST.value(), 431,"필수 파라미터값이 누락되었습니다."),
+
     ;
 
     private final Integer httpCode;
