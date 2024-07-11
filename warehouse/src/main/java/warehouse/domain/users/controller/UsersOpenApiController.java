575ea674c33ec0bde0535e5882b7c855b527feac
SEOB <svj7531@naver.com>
2024-07-11 14:14:14 +0900

    SB-165 (Refactor) : 긱 Controller에 @ValidApi 적용
    SB-165 (Refactor) : 긱 Controller에 @ValidApi 적용
    
    1. AddressApiController, ReceivingApiController, UserOpenApiController 에 기존에 존재하던 `customValidator.valid()` 를 제거하고 @ValidApi 적용

--- warehouse/src/main/java/warehouse/domain/users/controller/UsersOpenApiController.java
+++ warehouse/src/main/java/warehouse/domain/users/controller/UsersOpenApiController.java
@@ -9,7 +9,7 @@ import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestHeader;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
-import warehouse.common.customvalidation.CustomValidator;
+import global.annotation.ApiValid;
 import warehouse.domain.users.business.UsersBusiness;
 import warehouse.domain.users.controller.model.login.UserLoginRequest;
 import warehouse.domain.users.controller.model.register.UsersRegisterRequest;
@@ -23,22 +23,19 @@ import warehouse.domain.users.security.jwt.model.TokenResponse;
 public class UsersOpenApiController {
 
     private final UsersBusiness usersBusiness;
-    private final CustomValidator customValidator;
 
     @PostMapping()
     public Api<UsersRegisteredResponse> register(
-        @RequestBody Api<UsersRegisterRequest> request, Errors errors
+        @RequestBody @ApiValid Api<UsersRegisterRequest> request
     ) {
-        customValidator.validate(request, errors);
         UsersRegisteredResponse response = usersBusiness.register(request.getBody());
         return Api.OK(response);
     }
 
     @PostMapping("/login")
     public Api<TokenResponse> login(
-        @RequestBody Api<UserLoginRequest> request, Errors errors
+        @RequestBody @ApiValid Api<UserLoginRequest> request, Errors errors
     ) {
-        customValidator.validate(request, errors);
         TokenResponse response = usersBusiness.login(request.getBody());
         return Api.OK(response);
     }
