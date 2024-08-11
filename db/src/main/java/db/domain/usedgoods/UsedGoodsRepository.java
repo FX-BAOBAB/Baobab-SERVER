ebd083d5aeb9f2242f9a797585c3afa851e14d32
SEOB <svj7531@naver.com>
2024-08-12 01:44:36 +0900

    SB-244 (feat) : ChatRoom 기능 구현
    SB-244 (feat) : ChatRoom 기능 구현
    
    1. UsedGoodsRepository
    - findByUserId 메서드 추가

--- db/src/main/java/db/domain/usedgoods/UsedGoodsRepository.java
+++ db/src/main/java/db/domain/usedgoods/UsedGoodsRepository.java
@@ -1,11 +1,14 @@
 package db.domain.usedgoods;
 
 import db.domain.usedgoods.enums.UsedGoodsStatus;
+import java.util.List;
 import java.util.Optional;
 import org.springframework.data.jpa.repository.JpaRepository;
 
 public interface UsedGoodsRepository extends JpaRepository<UsedGoodsEntity, Long> {
 
     Optional<UsedGoodsEntity> findFirstByIdAndStatus(Long usedGoodsId, UsedGoodsStatus status);
+
+    List<UsedGoodsEntity> findByUserId(Long userId);
 
 }
