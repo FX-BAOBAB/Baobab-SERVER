91c6ab46f7bc6dbf1b8443f7d11c2ed50f51ce8a
JIUNG YANG <yangjiung286@gmail.com>
2024-08-22 20:50:22 +0900

    SB-261 (Feat) : Add Swagger Authorization

---
+++ users/src/main/java/users/common/config/web/WebConfig.java
@@ -0,0 +1,26 @@
+package users.common.config.web;
+
+import io.swagger.v3.oas.models.Components;
+import io.swagger.v3.oas.models.OpenAPI;
+import io.swagger.v3.oas.models.security.SecurityRequirement;
+import io.swagger.v3.oas.models.security.SecurityScheme;
+import java.util.Collections;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+
+@Configuration
+public class WebConfig implements WebMvcConfigurer {
+
+    @Bean
+    public OpenAPI swaggerAuthorization() {
+        SecurityScheme securityScheme = new SecurityScheme()
+            .type(SecurityScheme.Type.HTTP).scheme("bearer").bearerFormat("JWT")
+            .in(SecurityScheme.In.HEADER).name("Authorization");
+        SecurityRequirement securityRequirement = new SecurityRequirement().addList("bearerAuth");
+
+        return new OpenAPI()
+            .components(new Components().addSecuritySchemes("bearerAuth", securityScheme))
+            .security(Collections.singletonList(securityRequirement));
+    }
+}
