44186e8da8b3f4d05d91266c327a7ea595c3ed00
JIUNG YANG <yangjiung286@gmail.com>
2024-08-22 20:39:47 +0900

    SB-281 (Feat) : Users Module URL 지정

--- users/src/main/java/users/UsersApplication.java
+++ users/src/main/java/users/UsersApplication.java
@@ -5,7 +5,7 @@ import io.swagger.v3.oas.annotations.servers.Server;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
-//@OpenAPIDefinition(servers = {@Server(url = "https://baobab.run", description = "fx server 연습용 도메인")})
+@OpenAPIDefinition(servers = {@Server(url = "https://baobab.run/auth/", description = "Baobab Dev Server API")})
 @SpringBootApplication
 public class UsersApplication {
     public static void main(String[] args) {
