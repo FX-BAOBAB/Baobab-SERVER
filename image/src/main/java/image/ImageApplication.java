8bac1866ed4bc856694e8a6ca0e3868943dbccb7
JIUNG YANG <yangjiung286@gmail.com>
2024-08-30 14:49:47 +0900

    SB-288 (Fix) : Image URL 변경

--- image/src/main/java/image/ImageApplication.java
+++ image/src/main/java/image/ImageApplication.java
@@ -1,6 +1,8 @@
 package image;
 
 import image.domain.image.properties.FileStorageProperties;
+import io.swagger.v3.oas.annotations.OpenAPIDefinition;
+import io.swagger.v3.oas.annotations.servers.Server;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.context.properties.EnableConfigurationProperties;
@@ -8,6 +10,7 @@ import org.springframework.boot.context.properties.EnableConfigurationProperties
 @EnableConfigurationProperties({
     FileStorageProperties.class
 })
+@OpenAPIDefinition(servers = {@Server(url = "https://baobab.run/image/", description = "Baobab Dev Server API")})
 @SpringBootApplication
 public class ImageApplication {
 
