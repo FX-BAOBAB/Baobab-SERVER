37065d6deda878a4fc7c289000cdc249b5ea6d3e
JIUNG YANG <yangjiung286@gmail.com>
2024-09-02 19:26:25 +0900

    SB-292 (Feat) : Fault Swagger URL 지정

--- fault/src/main/java/fault/FaultApplication.java
+++ fault/src/main/java/fault/FaultApplication.java
@@ -1,8 +1,11 @@
 package fault;
 
+import io.swagger.v3.oas.annotations.OpenAPIDefinition;
+import io.swagger.v3.oas.annotations.servers.Server;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+@OpenAPIDefinition(servers = {@Server(url = "https://baobab.run/fault/", description = "Baobab Dev Server API")})
 @SpringBootApplication
 public class FaultApplication {
 
