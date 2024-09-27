2ab1dfef0401313965656329e9e9d82b574d95f9
shinyeongwoon <duddns119@naver.com>
2024-09-27 14:08:18 +0900

    SB-332 (docs) : delivery Swagger 요약 추가
    SB-332 (docs) : delivery Swagger 요약 추가
    
    1. Delivery Module Swagger 요약 설명 추가

--- delivery/src/main/java/delivery/domain/receiving/controller/ReceivingApiController.java
+++ delivery/src/main/java/delivery/domain/receiving/controller/ReceivingApiController.java
@@ -4,6 +4,7 @@ import delivery.domain.receiving.business.ReceivingBusiness;
 import delivery.domain.receiving.controller.model.ReceivingResponse;
 import delivery.domain.receiving.controller.model.ReceivingResponseList;
 import global.api.Api;
+import io.swagger.v3.oas.annotations.Operation;
 import io.swagger.v3.oas.annotations.Parameter;
 import java.time.LocalDate;
 import java.time.LocalDateTime;
@@ -25,24 +26,28 @@ public class ReceivingApiController {
     private final ReceivingBusiness receivingBusiness;
 
     @GetMapping
+    @Operation(summary = "[입고 요청서 목록 조회]")
     public Api<ReceivingResponseList> showReservationList() {
         ReceivingResponseList response = receivingBusiness.getReservationList();
         return Api.OK(response);
     }
 
     @GetMapping("/{requestId}")
+    @Operation(summary = "[입고 요청서 아이디로 입고 요청서 조회]")
     public Api<ReceivingResponse> showReservation(@PathVariable Long requestId) {
         ReceivingResponse response = receivingBusiness.getReservation(requestId);
         return Api.OK(response);
     }
 
     @PostMapping("/reservation/{requestId}")
+    @Operation(summary = "[입고 요청서 아이디로 입고 확정하기]")
     public Api<ReceivingResponse> receivingReservation(@PathVariable Long requestId) {
         ReceivingResponse response = receivingBusiness.reservationConfirmed(requestId);
         return Api.OK(response);
     }
 
     @GetMapping("/reservation")
+    @Operation(summary = "[입고 확정 상태 및 날짜 범위 내 입고 요청서 목록 조회]")
     public Api<ReceivingResponseList> showReservationByDate(
         @Parameter(hidden = true) @AuthenticationPrincipal User user, @RequestParam String date) {
         ReceivingResponseList response = receivingBusiness.showReservationByDate(date, user);
@@ -50,6 +55,7 @@ public class ReceivingApiController {
     }
 
     @PostMapping("/start/{requestId}")
+    @Operation(summary = "[입고 요청서 아이디로 배달 시작 상태로 변경]")
     public Api<ReceivingResponse> deliveryStart(
         @PathVariable Long requestId
     ) {
@@ -58,18 +64,21 @@ public class ReceivingApiController {
     }
 
     @PostMapping("/complete/{requestId}")
+    @Operation(summary = "[입고 요청서 아이디로 배달 완료 상태로 변경]")
     public Api<ReceivingResponse> deliveryComplete(@PathVariable Long requestId) {
         ReceivingResponse response = receivingBusiness.deliveryComplete(requestId);
         return Api.OK(response);
     }
 
     @PostMapping("/register/{requestId}")
+    @Operation(summary = "[입고 요청서 아이디로 요청서 접수 상태로 변경]")
     public Api<ReceivingResponse> registerRequest(@Parameter(hidden = true) @AuthenticationPrincipal User user,@PathVariable Long requestId) {
         ReceivingResponse response = receivingBusiness.registerRequest(user,requestId);
         return Api.OK(response);
     }
 
     @PostMapping("/check/{requestId}")
+    @Operation(summary = "[입고 요청서 아이디로 입고 심의 상태로 변경]")
     public Api<ReceivingResponse> checkStartRequest(@PathVariable Long requestId) {
         ReceivingResponse response = receivingBusiness.checkStartRequest(requestId);
         return Api.OK(response);
--- delivery/src/main/java/delivery/domain/shipping/controller/ShippingApiController.java
+++ delivery/src/main/java/delivery/domain/shipping/controller/ShippingApiController.java
@@ -6,6 +6,7 @@ import delivery.domain.shipping.business.ShippingBusiness;
 import delivery.domain.shipping.controller.model.ShippingResponse;
 import delivery.domain.shipping.controller.model.ShippingResponseList;
 import global.api.Api;
+import io.swagger.v3.oas.annotations.Operation;
 import io.swagger.v3.oas.annotations.Parameter;
 import lombok.RequiredArgsConstructor;
 import org.springframework.security.core.annotation.AuthenticationPrincipal;
@@ -25,18 +26,21 @@ public class ShippingApiController {
     private final ShippingBusiness shippingBusiness;
 
     @GetMapping
+    @Operation(summary = "[출고 요청서 목록 조회]")
     public Api<ShippingResponseList> showReservationList(){
         ShippingResponseList response = shippingBusiness.getReservationList();
         return Api.OK(response);
     }
 
     @GetMapping("/{requestId}")
+    @Operation(summary = "[출고 요청서 아이디로 출고 요청서 조회]")
     public Api<ShippingResponse> getShipping(@PathVariable Long requestId){
         ShippingResponse response = shippingBusiness.getReservation(requestId);
         return Api.OK(response);
     }
 
     @PostMapping("/reservation/{requestId}")
+    @Operation(summary = "[출고 신청서 아이디로 출고 확정 상태로 변경]")
     public Api<ShippingResponse> shippingReservation(
         @Parameter(hidden = true) @AuthenticationPrincipal User user,
         @PathVariable Long requestId
@@ -46,6 +50,7 @@ public class ShippingApiController {
     }
 
     @GetMapping("/reservation")
+    @Operation(summary = "[배달자 아이디와 날짜 범위 지정으로 출고 확정 상태의 출고 요청서 조회]")
     public Api<ShippingResponseList> showReservationByDate(
         @Parameter(hidden = true) @AuthenticationPrincipal User user,
         @RequestParam String date
@@ -55,6 +60,7 @@ public class ShippingApiController {
     }
 
     @PostMapping("/start/{requestId}")
+    @Operation(summary = "[출고 요청서 아이디로 배달 시작 상태로 변경]")
     public Api<ShippingResponse> deliveryStart(
         @PathVariable Long requestId
     ) {
@@ -63,10 +69,10 @@ public class ShippingApiController {
     }
 
     @PostMapping("/complete/{requestId}")
+    @Operation(summary = "[출고 요청서 아이디로 배달 완료 상태로 변경]")
     public Api<ShippingResponse> deliveryComplete(@PathVariable Long requestId) {
         ShippingResponse response = shippingBusiness.deliveryComplete(requestId);
         return Api.OK(response);
     }
-
 
 }
