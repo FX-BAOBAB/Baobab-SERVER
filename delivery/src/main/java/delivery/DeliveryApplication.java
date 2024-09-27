f8709ca27b21af31ad55245067f388356d26f829
shinyeongwoon <duddns119@naver.com>
2024-09-27 15:19:45 +0900

    SB-333 (feat) : 회원 가입 시 User Role 추가
    SB-333 (feat) : 회원 가입 시 User Role 추가
    
    1. User Role 추가
     -  MASER : 마스터,
        STORE_MANAGER : 관리자,
        BASIC_USER : 일반사용자,
        DELIVERY_MAN : 배송자
    2. 회원 가입 시 User Role filed 추가

--- delivery/src/main/java/delivery/DeliveryApplication.java
+++ delivery/src/main/java/delivery/DeliveryApplication.java
@@ -1,11 +1,16 @@
 package delivery;
 
+import delivery.common.properties.FileStorageProperties;
 import io.swagger.v3.oas.annotations.OpenAPIDefinition;
 import io.swagger.v3.oas.annotations.servers.Server;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.context.properties.EnableConfigurationProperties;
 
-@OpenAPIDefinition(servers = {@Server(url = "https://baobab.run", description = "fx server 연습용 도메인")})
+//@OpenAPIDefinition(servers = {@Server(url = "https://baobab.run", description = "fx server 연습용 도메인")})
+@EnableConfigurationProperties({
+    FileStorageProperties.class
+})
 @SpringBootApplication
 public class DeliveryApplication {
 
